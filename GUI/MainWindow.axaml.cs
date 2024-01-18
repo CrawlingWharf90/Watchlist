5513ea5b34cd373980e5c3858ebced1a7ab0f91f
Nadav Moscovici <71459855+CrawlingWharf90@users.noreply.github.com>
2024-01-18 12:35:16 +0100

    minor changes

--- GUI/MainWindow.axaml.cs
+++ GUI/MainWindow.axaml.cs
@@ -1,29 +1,15 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Dialogs;
 
-namespace GUI;
-
-public partial class MainWindow : Window
+namespace GUI
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
     }
-
-    public void Button_Click(object sender, RoutedEventArgs e)
-    {
-        ShowMessageBox();
-    }
-    private async void ShowMessageBox()
-    {
-        var dialog = new DialogBuilder().SetTitle("MessageBox Title")
-                                        .SetText("Hello, Avalonia!")
-                                        .SetIcon(DialogIcon.None)
-                                        .AddButton(DialogButton.Ok)
-                                        .Build();
-
-        await dialog.ShowDialog(this);
-    }
-
 }
